29887a7b5a8228a1bd85f7092af1b4afaa7f58c1
John Yeung <cfyeung2357@gmail.com>
2023-05-16 13:39:58 +0800

    added list of jobs and renders the home.html template on route '/'

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': "HK",
+    'salary': 'Rs. 100,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': "HK",
+    'salary': 'Rs. 100,000'
+  },
+]
 
 @app.route('/')
 
 def hello_world():
-  return "<p>Hello, World!</p>"
+  return render_template("home.html", jobs=JOBS, company_name = "Jovian")
 
 app.run(host="0.0.0.0", debug=True)
